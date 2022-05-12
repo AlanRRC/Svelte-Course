06fda9f246268e0d488eacdc75134de8778e3977
Alan Simpson <asimpson@rrc.ca>
2022-05-12 08:49:27 -0500

    using #each to loop

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,21 @@
 <script>
-	let firstName = 'Alan';
-	let lastName  = 'Simpson'
-	let beltColour = 'black'
-	
-	//	creates a reactive value
-	$: fullName = `${firstName} ${lastName}`
-
-	//	reactive statement
-	$: {
-		console.log(beltColour)
-		console.log(fullName)
-	}
-
-	const handleClick = () => {
-		beltColour = 'orange'
-	}
-
-	const handleInput = (e) => {
-		beltColour = e.target.value
-	}
+	let people = [
+		{ name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+		{ name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+		{ name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
+  	];	
 
 </script>
 
 <main>
-	<p>{fullName} {beltColour} belt</p>
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-	<input type="text" bind:value={beltColour}>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			<p>{person.age} years old, {person.beltColour} belt.</p>
+		</div>
+	{:else}
+		<p>There are no people to show...</p>
+	{/each}
 </main>
 
 <style>
@@ -35,13 +24,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
